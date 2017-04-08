4b727574625e2f45410c8d332890b00c51313381
Hector Urbina <hurbinas@gmail.com>
2017-04-08 16:44:16 -0300

    fix: change render's signature

--- easy_select2/widgets.py
+++ easy_select2/widgets.py
@@ -96,14 +96,14 @@ class Select2Mixin(object):
             return mark_safe(self.html.format(id=id_, options=options))
         return u''
 
-    def render(self, *args, **kwargs):
+    def render(self, name, value, attrs):
         """
         Extend base class's `render` method by appending
         javascript inline text to html output.
         """
-        output = super(Select2Mixin, self).render(*args, **kwargs)
-        id_ = kwargs['attrs']['id']
-        output += self.render_js_code(id_, *args, **kwargs)
+        output = super(Select2Mixin, self).render(name, value, attrs)
+        id_ = attrs['id']
+        output += self.render_js_code(id_, name, value, attrs)
 
         return mark_safe(output)
 
